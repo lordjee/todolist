de9cfa5569844e1a9ec8bda288286420391a930a
week2 <rkurniawan03@student.ciputra.ac.id>
2017-03-13 11:22:55 +0700

    initial commit

---
+++ resources/views/todo.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ToDo List</title>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+		<h1>ToDo List</h1>
+		<ul>
+			<li>Taki</li>
+			<li>Mitsuha</li>
+			<li>Yotsuha</li>
+		</ul>
+		<form>
+		<div class="row">
+			<div class="col-lg-13">
+				<div class="form-group">
+			<input type="text" class="form-control" name="todo_item" placeholder="Item baru...">
+				</div>
+			</div>
+			<div class="col-lg-1">	
+				<button type="submit" class="btn btn-success">Add</button>
+			</div>
+			<div class="col-lg-8">
+		</form>
+		<script scr="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		</div>
+		</div>
+		</div>
+</body>
+</html>
